b1740d48d295315bfe8b111a60aebca65b9f6891
Kyle McConnell <kylemcconnell17@gmail.com>
2022-03-27 11:42:25 -0500

    Update combine_txtfiles.py

--- combine_txtfiles.py
+++ combine_txtfiles.py
@@ -4,20 +4,17 @@ Created on Sun Mar 27 09:37:15 2022
 
 @author: Kyle
 """
+
+# import packages
 import os, glob
 
+# set directory for where your txt files are
 os.chdir('C:/Users/Kyle/Documents/GA Tech - OMS Analytics/ISYE6414/Module4/Transcripts')
 
 # get all txt files
 read_files = glob.glob("*.txt")
 
-# extract the number and create a tuple
-read_files = [(int(s.split("_")[0]), s) for s in read_files]
-
-# sort the tuple and create a new list with only the filename
-read_files = [x[1] for x in sorted(read_files)]
-
-
+# iterate and add all files back into a single file path
 with open("result_2.txt", "wb") as outfile:
     for f in read_files:
         with open(f, "rb") as infile:
